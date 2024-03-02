6e3369548315088e5854b9de3eb017356f15fdd1
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-03-02 17:26:25 -0600

    feat: use a local vector store instead of recreating it

--- backend.py
+++ backend.py
@@ -1,8 +1,11 @@
+import os
+
 from dotenv import load_dotenv
+
+from consts import OCP_PDF_PATH, OCP_INDEX_FILE_NAME, OCP_INDEX_DIR_PATH
 
 load_dotenv()
 
-from langchain_core.vectorstores import VectorStore
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
@@ -14,8 +17,24 @@ from typing_extensions import Any
 
 
 def get_indexed_vector_store() -> VectorStore:
+    if os.path.exists(OCP_INDEX_DIR_PATH + OCP_INDEX_FILE_NAME):
+        print("Loading existing index...")
+        load_local_vector_store()
+    else:
+        print(
+            f"Index {OCP_INDEX_DIR_PATH + OCP_INDEX_FILE_NAME} "
+            + "does not exist, creating..."
+        )
+        os.makedirs(OCP_INDEX_DIR_PATH, exist_ok=True)
+        return create_local_vector_store()
 
-    loader = PyPDFLoader("./ocp-study-guide.pdf")
+
+def load_local_vector_store() -> VectorStore:
+    FAISS.load_local(folder_path=OCP_INDEX_DIR_PATH, index_name=OCP_INDEX_FILE_NAME)
+
+
+def create_local_vector_store() -> VectorStore:
+    loader = PyPDFLoader(OCP_PDF_PATH)
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
     text_splitter = RecursiveCharacterTextSplitter(
@@ -27,7 +46,7 @@ def get_indexed_vector_store() -> VectorStore:
     return FAISS.from_documents(documents, embeddings)
 
 
-def run(vector_store: VectorStore, query: str) -> Any:
+def run_query(vector_store: VectorStore, query: str) -> Any:
 
     chat = ChatOpenAI(verbose=True, temperature=0)
     qa = RetrievalQA.from_chain_type(
---
+++ consts.py
@@ -0,0 +1,3 @@
+OCP_PDF_PATH = "./ocp-study-guide.pdf"
+OCP_INDEX_DIR_PATH = "./indexes/ocp-index/"
+OCP_INDEX_FILE_NAME = "ocp-index"
